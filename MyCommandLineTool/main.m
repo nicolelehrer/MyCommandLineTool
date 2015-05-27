9054cf4b58d6c84354b4fac1e2cca0a10d0b1233
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-05-27 14:08:46 -0400

    new project

---
+++ MyCommandLineTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyCommandLineTool
+//
+//  Created by Nicole Lehrer on 5/27/15.
+//  Copyright (c) 2015 Nicole Lehrer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
